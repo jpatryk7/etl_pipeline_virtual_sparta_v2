43b919801d4c36fa236492374299f15d739dcfce
jpatryk7 <pjesionka@spartaglobal.com>
2022-10-04 11:59:27 +0100

    Skeleton for tests

---
+++ app/pipeline/tests/test_extract_files.py
@@ -0,0 +1,22 @@
+import unittest
+from ..extract_files import *
+
+
+class TestExtractFiles(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test__txt_line_to_list(self) -> None:
+        pass
+
+    def test__get_file(self) -> None:
+        pass
+
+    def test__get_file_arbitrary_type(self) -> None:
+        pass
+
+    def test_check_for_new_files(self) -> None:
+        pass
+
+    def test_get_files_as_dataframe(self) -> None:
+        pass
