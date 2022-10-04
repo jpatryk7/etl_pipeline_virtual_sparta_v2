7129a555d3cf7e0abfd30d1cfba8ed00d48ad752
jpatryk7 <pjesionka@spartaglobal.com>
2022-10-04 17:43:15 +0100

    ...

--- app/pipeline/extract_files.py
+++ app/pipeline/extract_files.py
@@ -4,6 +4,29 @@ import pandas as pd
 import json
 
 
+def txt_line_to_list(line: str) -> list[str]:
+    """
+    Convert a line of text into a list. Specifically designed to deal with the known text files in the bucket of
+    interest.
+
+    :param str line: line of text
+    :return: list of elements in one row
+    :rtype: list[str]
+    """
+    semi_row = line.split('-')
+    # first few lines have less than three parts - they are describing the file, and we don't need them
+    if len(semi_row) == 2:
+        # first part is the name of the test participant
+        name = semi_row[0].strip(" ,\r\n")
+        # second part describes scores
+        tests = semi_row[1].split(',')
+        # split scores and remove names of each test
+        psychometrics_val = tests[0].split(':')[1].strip(" ,\r\n")
+        presentation_val = tests[1].split(':')[1].strip(" ,\r\n")
+        # column names are hard-coded later to reduce complexity of the code
+        return [name, psychometrics_val, presentation_val]
+
+
 class ExtractFiles:
     def __init__(self, bucket_name: str) -> None:
         """
@@ -16,35 +39,13 @@ class ExtractFiles:
         self.s3_resource = boto3.resource('s3')
         self.paginator = self.s3_client.get_paginator('list_objects_v2')
 
-    def _txt_line_to_list(self, line: str) -> list[str]:
-        """
-        Convert a line of text into a list. Specifically designed to deal with the known text files in the bucket of
-        interest.
-
-        :param str line: line of text
-        :return: list of elements in one row
-        :rtype: list[str]
-        """
-        semi_row = line.split('-')
-        # first few lines have less than three parts - they are describing the file, and we don't need them
-        if len(semi_row) == 2:
-            # first part is the name of the test participant
-            name = semi_row[0].strip(" ,\r\n")
-            # second part describes scores
-            tests = semi_row[1].split(',')
-            # split scores and remove names of each test
-            psychometrics_val = tests[0].split(':')[1].strip(" ,\r\n")
-            presentation_val = tests[1].split(':')[1].strip(" ,\r\n")
-            # column names are hard-coded later to reduce complexity of the code
-            return [name, psychometrics_val, presentation_val]
-
-    def _get_file(self, key: str) -> tuple[Any, str]:
+    def _get_file(self, key: str) -> Union[tuple[Any, str], None]:
         """
         Extracts a file from the S3 bucket with given filename.
 
         :param str key: name of the file to extract
         :return: dictionary / dataframe / nested list that contains data from the file
-        :rtype: tuple[Any, str]
+        :rtype: Union[tuple[Any, str], None]
         """
         # use different methods depending on the filetype
         if '.json' in key:
@@ -54,10 +55,12 @@ class ExtractFiles:
         elif '.txt' in key:
             file_bytes = self.s3_client.get_object(Bucket=self.bucket_name, Key=key)["Body"].readlines()
             # convert bytes to string and clean it using _txt_line_to_list
-            file_list = [self._txt_line_to_list(line.decode("utf-8")) for line in file_bytes]
+            file_list = [txt_line_to_list(line.decode("utf-8")) for line in file_bytes]
             return file_list, '.txt'
+        else:
+            return None
 
-    def _get_all_files_df(self, *, dtype: Union[Type[pd.DataFrame], Type[list]]) -> Union[pd.DataFrame, list[list[str]]]:
+    def _get_all_filenames_df(self, *, dtype: Union[Type[pd.DataFrame], Type[list]]) -> Union[pd.DataFrame, list[list[str]]]:
         """
         Returns all files in the bucket in one of the following formats:
             prefix      filename
@@ -77,14 +80,14 @@ class ExtractFiles:
         :return: dataframe with all prefixes and filenames in the S3 bucket
         :rtype: pd.DataFrame
         """
-        bucket_files = self.s3_resource.Bucket(self.bucket_name).objects.all()
+        bucket_files = [obj.key for obj in self.s3_resource.Bucket(self.bucket_name).objects.all()]
         files_list = [[filename.split('/')[0], filename.split('/')[1]] for filename in bucket_files]
         if dtype == list:
             return files_list
         else:
             return pd.DataFrame(files_list, columns=["prefix", "filename"])
 
-    def get_files_as_dataframe(self, recorded_files: list[str]) -> tuple:
+    def get_files_as_df(self, recorded_files: list[str]) -> tuple:
         """
         Gather any files that are in S3 that were not recorded before.
 
@@ -92,12 +95,12 @@ class ExtractFiles:
         :return: tuple of dataframes containing all .json, .csv, .txt files and dataframe with newly discovered files
         :rtype: tuple
         """
-        file_dict = self._get_all_files_df(dtype=list)
+        file_df = self._get_all_filenames_df(dtype=pd.DataFrame)
 
         files_dict = {'.json': [], '.csv': [], '.txt': []}
         new_filenames = []
 
-        for _, row in file_dict.iterrows():
+        for _, row in file_df.iterrows():
             if f"{row['prefix']}/{row['filename']}" not in recorded_files:
                 file, ftype = self._get_file(row["filename"])
                 files_dict[ftype].append(file)
---
+++ app/pipeline/test_extract_files.py
@@ -0,0 +1,112 @@
+import unittest
+
+import pandas as pd
+
+from app.pipeline.extract_files import *
+
+
+class TestExtractFiles(unittest.TestCase):
+
+    # speeds up running tests
+    all_items_in_s3 = [obj.key for obj in boto3.resource('s3').Bucket("data32-final-project-files").objects.all()]
+
+    def setUp(self) -> None:
+        self.bucket_name = "data32-final-project-files"
+        self.extract = ExtractFiles(self.bucket_name)
+        self.json_filename = "Talent/10397.json"
+        self.csv_filename = "Talent/March2019Applicants.csv"
+        self.txt_filename = "Talent/Sparta Day 6 February 2019.txt"
+
+    def test__txt_line_to_list(self) -> None:
+        """
+        E.g.
+        input: "ROLLINS DALGARDNO -  Psychometrics: 50/100, Presentation: 22/32"
+        expected output: ["ROLLINS DALGARDNO", "50/100", "22/32"]
+        """
+        expected = ["ROLLINS DALGARDNO", "50/100", "22/32"]
+        actual = txt_line_to_list("ROLLINS DALGARDNO -  Psychometrics: 50/100, Presentation: 22/32")
+        self.assertEqual(expected, actual)
+
+    def test__get_file_json(self) -> None:
+        """
+        check if the returned file is a dict type
+        """
+        actual = self.extract._get_file(self.json_filename)[0]
+        self.assertIsInstance(actual, dict)
+
+    def test__get_file_csv(self) -> None:
+        """
+        check if the returned file is a dataframe type
+        """
+        actual = self.extract._get_file(self.csv_filename)[0]
+        self.assertIsInstance(actual, pd.DataFrame)
+
+    def test__get_file_txt(self) -> None:
+        """
+        check if the returned file is a nested list
+        """
+        actual = self.extract._get_file(self.txt_filename)[0]
+        self.assertIsInstance(actual, list)
+
+    def test__get_file_unknown_type(self) -> None:
+        """
+        Check if the function returns None when provided with unexpected filetype.
+        """
+        expected = None
+        actual = self.extract._get_file("some_filename.png")
+        self.assertEqual(expected, actual)
+
+    def test__get_all_files_df_dataframe(self) -> None:
+        """
+        check if the returned type is matching the specified
+        """
+        actual = self.extract._get_all_filenames_df(dtype=pd.DataFrame)
+        self.assertIsInstance(actual, pd.DataFrame)
+
+    def test__get_all_files_df_list(self) -> None:
+        """
+        check if the returned type is matching the specified
+        """
+        actual = self.extract._get_all_filenames_df(dtype=list)
+        self.assertIsInstance(actual, list)
+
+    def test__get_all_files_df_list_len(self) -> None:
+        """
+        check if the number of elements is correct
+        """
+        expected = len(self.all_items_in_s3)
+        actual = len(self.extract._get_all_filenames_df(dtype=list))
+        self.assertEqual(expected, actual)
+
+    def test__get_all_files_df_dataframe_len(self) -> None:
+        """
+        check if the number of elements is correct
+        """
+        expected = len(self.all_items_in_s3)
+        actual = len(self.extract._get_all_filenames_df(dtype=pd.DataFrame))
+        self.assertEqual(expected, actual)
+
+    def test_get_files_as_df_new_file(self) -> None:
+        """
+        Check if the function returns all 4 dataframes /w one entry in new_filenames_df when provided recorded_files
+        having one less file than all_items_in_s3
+        """
+        expected = self.all_items_in_s3[0]
+        actual = '/'.join(self.extract.get_files_as_df(self.all_items_in_s3[1:])[3].values.tolist()[0])
+        self.assertEqual(expected, actual)
+
+    def test_get_files_as_df_no_recorder_files(self) -> None:
+        """
+        Check if the function returns all_items_in_s3 when provided empty recorded_files
+        """
+        expected = len(self.all_items_in_s3)
+        actual = len(self.extract.get_files_as_df([])[3])
+        self.assertEqual(expected, actual)
+
+    def test_get_files_as_df_all_files_recorded(self) -> None:
+        """
+        Check if the function returns None when provided recorded_files same as all_items_in_s3
+        """
+        expected = None
+        actual = self.extract.get_files_as_df(self.all_items_in_s3)
+        self.assertEqual(expected, actual)
