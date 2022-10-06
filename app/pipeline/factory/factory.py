64382f9328320331b39588147ff590d5ea9702ed
jpatryk7 <pjesionka@spartaglobal.com>
2022-10-06 17:46:46 +0100

    Fixed some bugs

--- app/pipeline/factory/factory.py
+++ app/pipeline/factory/factory.py
@@ -1,12 +1,30 @@
-from .seed_db import SeedDB
+from .seed_db import SeedDB, many_to_many_relationship_generator
+from ..models import TechScore, Strength, Weakness
 
 
 def standard_factory() -> None:  # pragma: no cover
     seed = SeedDB()
-    seed.trainer()
-    seed.course()
+
+    # generate some TechScore, Strength, Weakness entries
     for _ in range(10):
+        seed.tech_score()
+        seed.strength()
+        seed.weakness()
+
+    # generate some trainers and courses
+    for _ in range(5):
+        seed.trainer()
+        seed.course()
+
+    # generate some students with invitation, test score and academy performance data
+    for _ in range(25):
         seed.invitation()
         seed.test_score()
         seed.academy_performance()
+        seed.trainee_performance()
         seed.student_information()
+
+    # build many-to-many relationships
+    many_to_many_relationship_generator('tech_scores', TechScore, null=False)
+    many_to_many_relationship_generator('strengths', Strength, null=False)
+    many_to_many_relationship_generator('weaknesses', Weakness, null=False)
--- app/pipeline/factory/seed_db.py
+++ app/pipeline/factory/seed_db.py
@@ -1,3 +1,4 @@
+from typing import Type, Union, Any
 from faker import Faker
 import random
 from ..models import (
@@ -14,26 +15,85 @@ from ..models import (
 )
 
 
+def many_to_many_relationship_generator(
+        field_name: str,
+        other_model: Type[Union[TechScore, Strength, Weakness]],
+        *, null: bool = True,
+        max_relations: int = 5) -> None:
+    # list of all TraineePerformance objects and objects to connect to (e.g. all TechScore entries)
+    trainee_performance_obj_all = list(TraineePerformance.objects.all())
+    other_model_obj_all = list(other_model.objects.all())
+
+    # restrict max number of other_object related to the TraineePerformance object
+    if len(other_model_obj_all) < max_relations:
+        max_relations = len(other_model_obj_all)
+
+    # random number of relations to generate (e.g. if numbers are 5 and 8 then 5 TraineePerformance entries will
+    # have a relation with 8 other_model entries)
+    trainee_performance_no_of_relations = random.randint(1, len(trainee_performance_obj_all)) if null else len(trainee_performance_obj_all)
+    other_model_no_of_relations = random.randint(1, max_relations)
+
+    trainee_performance_subset = random.sample(trainee_performance_obj_all, trainee_performance_no_of_relations) if null else trainee_performance_obj_all
+    other_model_subset = random.sample(other_model_obj_all, other_model_no_of_relations)
+
+    for trainee_performance_obj in trainee_performance_subset:
+        if field_name == "tech_scores":
+            trainee_performance_obj.tech_scores.add(*other_model_subset)
+        elif field_name == "strengths":
+            trainee_performance_obj.strengths.add(*other_model_subset)
+        elif field_name == "weaknesses":
+            trainee_performance_obj.weaknesses.add(*other_model_subset)
+        else:
+            raise Exception(f"field_name must be 'tech_scores', 'strengths', 'weaknesses'. Got {field_name} instead.")
+
+
+class IDManager:
+    """
+    Simple ID helper class to keep track of one-to-one relations
+    """
+    def __init__(self):
+        self.available_ids = {
+            'test_score_id': [],
+            'invitation_id': [],
+            'academy_performance_id': [],
+            'trainee_performance_id': []
+        }
+
+    def add_id(self, field_name: str, _id: int) -> None:
+        self.available_ids[field_name].append(_id)
+
+    def remove_id(self, field_name: str, _id: int) -> None:
+        self.available_ids[field_name].remove(_id)
+
+    def get_random_id(self, field_name: str, remove: bool = True) -> int:
+        random_id = random.choice(self.available_ids[field_name])
+        if remove:
+            self.remove_id(field_name, random_id)
+        return random_id
+
+    def get_ids(self, field_name: str) -> list[int]:
+        return self.available_ids[field_name]
+
+
 class SeedDB:  # pragma: no cover
     def __init__(self) -> None:
         self.faker = Faker('en_US')
+        self.id_manager = IDManager()
 
     def student_information(self, *, null: bool = False) -> None:
         if null:
-            university, degree, test_score_id, invitation_id, course_id, academy_performance_id = (None, None, None, None, None, None)
+            university, degree, test_score_id, invitation_id, course_id, academy_performance_id, trainee_performance_id = (None, None, None, None, None, None, None)
         else:
             university = self.faker.text(max_nb_chars=10)
             degree = self.faker.text(max_nb_chars=10)
 
-            test_score_id_all = TestScore.objects.all().values_list('id', flat=True)
-            invitation_id_all = Invitation.objects.all().values_list('id', flat=True)
             course_id_all = Course.objects.all().values_list('id', flat=True)
-            academy_performance_id_all = AcademyPerformance.objects.all().values_list('id', flat=True)
 
-            test_score_id = TestScore.objects.get(pk=random.choice(test_score_id_all))
-            invitation_id = Invitation.objects.get(pk=random.choice(invitation_id_all))
+            test_score_id = TestScore.objects.get(pk=self.id_manager.get_random_id('test_score_id'))
+            invitation_id = Invitation.objects.get(pk=self.id_manager.get_random_id('invitation_id'))
             course_id = Course.objects.get(pk=random.choice(course_id_all))
-            academy_performance_id = AcademyPerformance.objects.get(pk=random.choice(academy_performance_id_all))
+            academy_performance_id = AcademyPerformance.objects.get(pk=self.id_manager.get_random_id('academy_performance_id'))
+            trainee_performance_id = TraineePerformance.objects.get(pk=self.id_manager.get_random_id('trainee_performance_id'))
 
         student_information_obj = StudentInformation(
             name=self.faker.text(max_nb_chars=20),
@@ -48,28 +108,36 @@ class SeedDB:  # pragma: no cover
             test_score_id=test_score_id,
             invitation_id=invitation_id,
             course_id=course_id,
-            academy_performance_id=academy_performance_id
+            academy_performance_id=academy_performance_id,
+            trainee_performance_id=trainee_performance_id
         )
 
         student_information_obj.save()
 
     def trainee_performance(self, *, null: bool = False) -> None:
         if null:
-            student_information_id, course_interest = (None, None)
+            course_interest = None
         else:
-            student_information_id_all = StudentInformation.objects.all().values_list('id', flat=True)
-            student_information_id = StudentInformation.objects.get(pk=random.choice(student_information_id_all))
+            course_interest = self.faker.text(max_nb_chars=10)
 
         trainee_performance_obj = TraineePerformance(
-            student_information_id=student_information_id,
-            date = self.faker.date(),
+            date=self.faker.date(),
             self_development=self.faker.boolean(),
             geo_flex=self.faker.boolean(),
             financial_support=self.faker.boolean(),
-            course_interest=self.faker.text(max_nb_chars=10)
+            course_interest=course_interest
         )
 
         trainee_performance_obj.save()
+
+        # add many-to-many relationships
+        tech_score_obj_all = TechScore.objects.all()
+        tech_score_no_of_relations = random.randint(0, len(tech_score_obj_all))
+        for i in range(tech_score_no_of_relations):
+            trainee_performance_obj.tech_scores.add(tech_score_obj_all[i])
+
+        # save id
+        self.id_manager.add_id('trainee_performance_id', trainee_performance_obj.pk)
 
     def academy_performance(self, *, null: bool = False) -> None:
         if null:
@@ -86,6 +154,7 @@ class SeedDB:  # pragma: no cover
             )
 
         academy_performance_obj.save()
+        self.id_manager.add_id('academy_performance_id', academy_performance_obj.pk)
 
     def course(self, *, null: bool = False) -> None:
         if null:
@@ -104,7 +173,7 @@ class SeedDB:  # pragma: no cover
 
     def trainer(self) -> None:
         trainer_obj = Trainer(
-            name=self.faker.text(max_nb_chars=200)
+            name=self.faker.text(max_nb_chars=20)
         )
 
         trainer_obj.save()
@@ -116,6 +185,7 @@ class SeedDB:  # pragma: no cover
         )
 
         test_score_obj.save()
+        self.id_manager.add_id('test_score_id', test_score_obj.pk)
 
     def strength(self) -> None:
         strength_obj = Strength(strength=self.faker.text(max_nb_chars=5))
@@ -139,6 +209,7 @@ class SeedDB:  # pragma: no cover
         )
 
         invitation_obj.save()
+        self.id_manager.add_id('invitation_id', invitation_obj.pk)
 
     def tech_score(self) -> None:
         tech_score_obj = TechScore(
