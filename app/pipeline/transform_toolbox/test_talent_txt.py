751bc1d35b019e6a90b171a3207bd676a08c0a31
jomar599 <jomar@spartaglobal.com>
2022-10-12 15:40:46 +0100

    Transformed the Text files to add date column and tested.

--- app/pipeline/transform_toolbox/test_talent_txt.py
+++ app/pipeline/transform_toolbox/test_talent_txt.py
@@ -1,35 +1,60 @@
 import unittest
 from pathlib import Path
 import pandas as pd
-from .talent_txt import TalentTXT
-from .date_format_test import date_format_test
+import datetime
+from date_format_test import date_format_test
+from talent_txt import TalentTXT
 
 
 class TestTalentTXT(unittest.TestCase):
     def setUp(self) -> None:
         self.TalentTXT = TalentTXT()
         self.pickle_jar_path = Path(__file__).resolve().parent.parent / "pickle_jar"
-        self.raw_df = pd.read_pickle(self.pickle_jar_path / "talent_txt.pkl")
+        self.raw_df = pd.read_pickle(self.pickle_jar_path / "talent_txt_v2.pkl")
         self.filenames = pd.read_pickle(self.pickle_jar_path / "talent_txt_filenames.pkl")
 
-    def test_replace_hyphens(self):
-        expected = "DUN PEACHMENT ,  Psychometrics: 45/100, Presentation: 19/32"
-        actual = self.TalentTXT.replace_hyphens("DUN PEACHMENT -  Psychometrics: 45/100, Presentation: 19/32")
-        self.assertEqual(actual,expected)
 
-    def test_replace_colon(self):
-        expected = "DUN PEACHMENT -  Psychometrics, 45/100, Presentation, 19/32"
-        actual = self.TalentTXT.replace_colon("DUN PEACHMENT -  Psychometrics: 45/100, Presentation: 19/32")
-        self.assertEqual(actual,expected)
+    def test_remove_word_column(self):
+        df = pd.DataFrame({'words': ['Sparta Day Hi', 'Sparta Day Hello']})
+        expected = pd.DataFrame({'words': [' Hi', ' Hello']})
+        actual = self.TalentTXT.remove_word(df, 'words','Sparta Day')
+        self.assertEqual(expected['words'][1],actual['words'][1])
 
-    def test_delete_spaces(self):
-        expected = ""
+    def test_transform_date(self):
+        df = pd.DataFrame({'date': ['3 May 2015','2 March 2016']})
+        expected_df = pd.DataFrame({'date': [[3,5,2015], [2,3,2016]]})
+        actual = self.TalentTXT.transform_date(df, 'date')
+        self.assertEqual(expected_df['date'][0], actual['date'][0])
+
+    def test_rename_column(self):
+        df = pd.DataFrame({'words': ['Sparta Day Hi', 'Sparta Day Hello'], 'year' : [2015,2016]})
+        expected = pd.DataFrame({'stuff': ['Sparta Day Hi', 'Sparta Day Hello'], 'year' : [2015,2016]})
+        actual = self.TalentTXT.rename_column(df, 'words', 'stuff')
+        self.assertEqual(expected.columns[0],actual.columns[0])
+
+    def test_reorder_column(self):
+        df = pd.DataFrame({'words': ['Sparta Day Hi', 'Sparta Day Hello'], 'year' : [2015,2016]})
+        expected = pd.DataFrame({'year':[2015,2016], 'words':['Sparta Day Hi', 'Sparta Day Hello']})
+        actual = self.TalentTXT.reorder_columns(df, ['year','words'])
+        self.assertEqual(expected.columns[0],actual.columns[0])
+
+    def test_lower_case(self):
+        df = pd.DataFrame({'words': ['SPARTA Day Hi', 'Sparta Day Hello'], 'year': [2015, 2016]})
+        expected = pd.DataFrame({'words': ['Sparta Day Hi', 'Sparta Day Hello'], 'year': [2015, 2016]})
+        actual = self.TalentTXT.lower_case(df, 'words')
+        self.assertEqual(expected['words'][0],actual['words'][0])
+
+    def test_del_duplicates(self):
+        df = pd.DataFrame({'words': ['Sparta Day Hi','Sparta Day Hi', 'Sparta Day Hello'], 'year': [2015,2015, 2016]})
+        expected = pd.DataFrame({'words': ['Sparta Day Hi', 'Sparta Day Hello'], 'year': [2015, 2016]})
+        actual = self.TalentTXT.del_duplicates(df)
+        self.assertEqual(len(expected.columns),len(actual.columns))
 
 
 class TestTransformAcademyCSV(unittest.TestCase):
     def setUp(self) -> None:
         self.pickle_jar_path = Path(__file__).resolve().parent.parent / "pickle_jar"
-        self.raw_df = pd.read_pickle(self.pickle_jar_path / "talent_txt.pkl")
+        self.raw_df = pd.read_pickle(self.pickle_jar_path / "talent_txt_v2.pkl")
         self.talent_txt_transform = TalentTXT()
         self.test_score_df = self.talent_txt_transform.transform_talent_txt(self.raw_df)
 
@@ -38,7 +63,7 @@ class TestTransformAcademyCSV(unittest.TestCase):
     ############################
 
     def test_transform_talent_txt_test_score_df_col_names(self) -> None:
-        expected = {"student_name", "date", "psychometrics", "presentations"}
+        expected = {"student_name", "date", "psychometrics", "presentation"}
         actual = set(self.test_score_df.columns.tolist())
         self.assertEqual(expected, actual)
 
